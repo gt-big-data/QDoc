eb21850afcadef1eb8c79647356da3a6411bc007
Sam Marder <smarder3@gatech.edu>
2015-10-02 18:42:22 -0400

    Made isValid into a member function of Article.

--- article.py
+++ article.py
@@ -1,6 +1,9 @@
 import json
 from dbco import * # this imports the db connection
 
+def good(val):
+    return val and len(val) > 0
+
 class Article:
     def __new__(self, guid='', title='', url='', timestamp=0, source='', feed=''):
         self.guid = guid
@@ -13,11 +16,30 @@ class Article:
         self.img = ''
         self.keywords = []
 
+    def isValid(self):
+        """Check if the article has enough data to be considered "crawled"."""
+
+        if not good(self.guid):
+            return False
+        if not good(self.title):
+            return False
+        if not good(self.url):
+            return False
+        if self.timestamp < 500:
+            return False
+        if not good(self.source):
+            return False
+        if not good(self.feed):
+            return False
+        if not good(self.content):
+            return False
+        return True
+
 def saveNewArticles(newArticles):
     """Add valid articles to the database."""
     As = []
     for a in newArticles:
-        if isValid(a):
+        if a.isValid():
             As.append(a._asdict())
         else:
             print "Article from source: ", a.source, "feed: ", a.feed, " was invalid"
@@ -34,30 +56,7 @@ def saveNewArticlesFile(newArticles):
     """Write an array of articles to DB_ex.txt"""
     with open("DB_ex.txt", "a") as f:
         for a in newArticles:
-            if isValid(a):
+            if a.isValid():
                 f.write(json.dumps(a._asdict())+'\n')
             else:
                 print "Article from source: ", a.source, "feed: ", a.feed, " was invalid"
-
-def good(val):
-    return val and len(val) > 0
-
-# TODO: Make this a member function.
-def isValid(a):
-    """Check if the article has enough data to be considered "crawled"."""
-
-    if not good(a.guid):
-        return False
-    if not good(a.title):
-        return False
-    if not good(a.url):
-        return False
-    if a.timestamp < 500:
-        return False
-    if not good(a.source):
-        return False
-    if not good(a.feed):
-        return False
-    if not good(a.content):
-        return False
-    return True
