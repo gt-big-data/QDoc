51acd045ec890940ecb27f675cae3a95fd16b0bb
Philippe Laban <plaban3@gatech.edu>
2015-12-19 21:22:05 +0100

    New recrawlBatch with eventlet (async), way faster

--- recrawlBatch.py
+++ recrawlBatch.py
@@ -2,15 +2,18 @@ from bson.objectid import ObjectId
 from crawlContent import *
 from article import *
 from dbco import *
-import sys, socket
+import sys, socket, eventlet
+from eventlet.green import urllib2
 socket.setdefaulttimeout(5)
 
-def recrawlArt(art, article):
+def recrawlArt(art):
 	try:
 		html = urllib2.urlopen(art['url']).read()
 	except:
 		print "Error 404: Not Found"
-		return None
+		return {'id': art['_id'], 'content': None}
+
+	article = Article(guid=art['guid'], title=art['title'], url=art['url'], timestamp=art['timestamp'], source=art['source'], feed=art['feed'])
 	soup = htmlToSoup(article, html)
 	parse(article, html)
 
@@ -21,22 +24,33 @@ def recrawlArt(art, article):
 	print "-------------------------"
 	print art['_id']
 	print "Old: ", len(oldContent), " | New: ", len(newContent)
-	return newContent
+	return {'id': art['_id'], 'content': newContent}
 
 def recrawlSource(source=None):
-	left = 1
+	left = db.qdoc.find({'recrawl': {'$exists': True}}).count()
 	while left>0:
 		sort = -1
 		articles = list(db.qdoc.find({'recrawl': {'$exists': True}}).sort('timestamp', sort).limit(50))
 
 		qdocUpdate = db.qdoc.initialize_unordered_bulk_op()
-		for art in articles:
-			article = Article(guid=art['guid'], title=art['title'], url=art['url'], timestamp=art['timestamp'], source=art['source'], feed=art['feed'])
-			newContent = recrawlArt(art, article)
-			if newContent:
-				qdocUpdate.find({'_id': art['_id']}).upsert().update({'$set': {'content': newContent}, '$unset': {'recrawl': True}})
-			else:
-				qdocUpdate.find({'_id': art['_id']}).upsert().update({'$unset': {'recrawl': True}})
+
+		batch = []
+		i = 0
+		while i < len(articles):
+			while len(batch) < min(20, left):
+				batch.append(articles[i])
+				i += 1
+
+			pool = eventlet.GreenPool()
+
+			for ret in pool.imap(recrawlArt, batch):
+				if ret['content']:
+					print len(ret['content'])
+					qdocUpdate.find({'_id': ret['id']}).upsert().update({'$set': {'content': ret['content']}, '$unset': {'recrawl': True}})
+				else:
+					qdocUpdate.find({'_id': ret['id']}).upsert().update({'$unset': {'recrawl': True}})
+			left -= len(batch)
+			batch = []
 
 		qdocUpdate.execute()
 		left = db.qdoc.find({'recrawl': {'$exists': True}}).count()
