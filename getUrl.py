b128a8aa48f5f73bbbe32badfe422094fbd93866
Sam Marder <smarder3@gatech.edu>
2016-02-05 10:06:05 -0500

    Removed dependency on eventlet.

--- getUrl.py
+++ getUrl.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup, Comment, Doctype, NavigableString
 import concurrent.futures as futures # for multithreading
-import sys, eventlet, time, warnings, requests
+import sys, time, warnings, requests
 from dbco import *
 
 warnings.filterwarnings("ignore")
@@ -20,15 +20,8 @@ def getURLs(urls):
 	with futures.ThreadPoolExecutor(max_workers=100) as executor:
 		return executor.map(getUrl, urls)
 
-def getURLsEventlet(urls): # this is legacy now, it is slower...
-	pool = eventlet.GreenPool()
-	return [ret for ret in pool.imap(getUrl, urls)]
-
 if __name__ == '__main__':
 	urls = [a['url'] for a in list(db.qdoc.find({}, {'url': True}).limit(1200))]
 	time2 = time.time()
 	bla= getURLs(urls)
 	print "MULTITHREADING : ", (time.time()-time2) ,"s"
-	time1 = time.time()
-	bla= getURLsEventlet(urls)
-	print "EVENTLET METHOD: ", (time.time()-time1) ,"s"
