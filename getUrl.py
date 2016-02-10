e36b08a1c702c7b613c390a81df7c0d21ed68643
Sam Marder <smarder3@gatech.edu>
2016-02-10 11:35:41 -0500

    Cleaned up getUrl.py.

--- getUrl.py
+++ getUrl.py
@@ -1,18 +1,17 @@
-from bs4 import BeautifulSoup, Comment, Doctype, NavigableString
+from bs4 import BeautifulSoup
 import concurrent.futures as futures # for multithreading
-import sys, time, warnings, requests
-from dbco import *
+import warnings, requests
 
 warnings.filterwarnings("ignore")
 
 def getUrl(url):
-	time1 = time.time()
 	try:
 		req = requests.get(url, allow_redirects=True, verify=False,timeout=10)
 	except Exception as e:
 		status_code = str(e)
 		return {'soup': BeautifulSoup('', 'html.parser'), 'finalURL': '', 'error': status_code} # some error happened
 
+	# TODO: Why are we doing this replacement? I'd be surprised if BeautifulSoup can't handle both types of tags.
 	html = (req.text).replace('<br>', '<br />')
 	return {'soup': BeautifulSoup(html, 'html.parser'), 'finalURL': req.url}
 
@@ -23,9 +22,3 @@ def getURLs(urls):
 		# ThreadPool shutting down before all of the tasks finishing.
 		downloaded_urls = [d for d in downloaded_urls]
 	return downloaded_urls
-
-if __name__ == '__main__':
-	urls = [a['url'] for a in list(db.qdoc.find({}, {'url': True}).limit(1200))]
-	time2 = time.time()
-	bla= getURLs(urls)
-	print "MULTITHREADING : ", (time.time()-time2) ,"s"
