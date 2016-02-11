c78e3ebb05ecc74081b02e9e2af9c5fc66fe2431
Philippe Laban <plaban3@gatech.edu>
2016-02-11 13:11:15 -0500

    Cleaned up

--- dbco.py
+++ dbco.py
@@ -1,4 +1,8 @@
 from pymongo import MongoClient, errors
+import os
+# Set to 'api.retinanews.net' in production.
+host = os.environ.get('QDOC_MONGO_HOST') or 'localhost'
+print('Connecting to Mongo at %s' % host)
 
-client = MongoClient('mongodb://143.215.138.132:27017/') # mongodb://146.148.59.202:27017/ old GCE 
+client = MongoClient('mongodb://%s:27017/' % host) # mongodb://146.148.59.202:27017/ old GCE
 db = client['big_data']
