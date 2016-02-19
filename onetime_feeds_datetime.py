7e44d3e03dbc9322cdb623d4a161ab9626bffd46
Sam Marder <smarder3@gatech.edu>
2016-02-19 09:50:07 -0500

    Switch existing articles to ISODate objects.

--- onetime_feeds_datetime.py
+++ onetime_feeds_datetime.py
@@ -3,6 +3,23 @@
 import db
 from datetime import datetime
 
+
+def fixArticleIntStamps():
+    bulkUpdate = db.qdoc.initialize_unordered_bulk_op()
+    query = db.qdoc.find({'$or': [
+        {'timestamp': {'$type': 'int'}},
+        {'timestamp': {'$type': 'double'}},
+    ]})
+    shouldExecute = False
+    for article in query:
+        shouldExecute = True
+        stamp = article.get('timestamp', 0)
+        date = datetime.utcfromtimestamp(stamp)
+        bulkUpdate.find({'timestamp': stamp}).update({'$set': {'timestamp': date}})
+
+    if shouldExecute:
+        bulkUpdate.execute()
+
 def fixIntStamps():
     bulkUpdate = db.feed.initialize_unordered_bulk_op()
     query = db.feed.find({'$or': [
@@ -13,7 +30,6 @@ def fixIntStamps():
     for feed in query:
         shouldExecute = True
         stamp = feed.get('stamp', 0)
-        print stamp
         date = datetime.utcfromtimestamp(stamp)
         bulkUpdate.find({'stamp': stamp}).update({'$set': {'stamp': date}})
 
@@ -30,12 +46,15 @@ def fixIntLastCrawl():
     for feed in query:
         shouldExecute = True
         crawl = feed.get('lastCrawl', 0)
-        print crawl
         date = datetime.utcfromtimestamp(crawl)
         bulkUpdate.find({'lastCrawl': crawl}).update({'$set': {'lastCrawl': date}})
 
     if shouldExecute:
         bulkUpdate.execute()
 
+print 'Fixing feed timestamps.'
 fixIntStamps()
+print 'Fixing feed lastcrawl timestamps.'
 fixIntLastCrawl()
+print 'Fixing article timestamps.'
+fixArticleIntStamps()
