a6eabc42fdaf9c716bdf5fdf2febf64050239381
Xiaochuang Han <Xiaochuang@lawn-143-215-51-235.lawn.gatech.edu>
2016-02-26 16:49:16 -0500

    New Twitter Crawler

--- twitter/download.py
+++ twitter/download.py
@@ -4,32 +4,64 @@ from tweet import *
 class TweetSampler(twython.TwythonStreamer):
 
     def on_success(self, data):
-        if 'text' in data and 'id_str' in data and 'user' in data and 'id_str' in data['user'] and 'timestamp_ms' in data and 'place' in data and 'id' in data['place']:
+        if 'text' in data and 'id_str' in data and 'user' in data and 'id_str' in data['user'] and 'name' in data['user'] and 'followers_count' in data['user'] and 'timestamp_ms' in data and 'entities' in data:
             time = int(float(data['timestamp_ms']) / 1000.0)
-            words, hashtags, mentions, count = cleanTweet(data['text'])
-            longitude = 0
-            latitude = 0
-            if 'bounding_box' in data['place']:
-                bbox = data['place']['bounding_box']['coordinates'][0]
-                longitude = ((bbox[0][0]+bbox[2][0])/2.0)
-                latitude = ((bbox[0][1]+bbox[1][1])/2.0)
-            T = Tweet(data['id_str'], data['text'], data['user']['id_str'], time, longitude, latitude, words, hashtags, mentions)
-            saveNewTweet(T, count)
+            hashtags = []
+            hashtagsIndices = []
+            for hashtag in data['entities']['hashtags']:
+                hashtags.append(hashtag['text'])
+                hashtagsIndices.append(hashtag['indices'])
+            mentions_id = []
+            mentions_name = []
+            mentionsIndices = []
+            for mention in data['entities']['user_mentions']:
+                mentions_id.append(mention['id_str'])
+                mentions_name.append(mention['name'])
+                mentionsIndices.append(mention['indices'])
+            urls = []
+            urlsIndices = []
+            for url in data['entities']['urls']:
+                urls.append(url['url'])
+                urlsIndices.append(url['indices'])
+            words, keywords = summarizeTweet(data['text'], hashtagsIndices, mentionsIndices, urlsIndices)
+            favorite_count = 0
+            retweet_count = 0
+            if 'favorite_count' in data:
+                favorite_count = data['favorite_count']
+            if 'retweet_count' in data:
+                retweet_count = data['retweet_count']
+            lon = 0
+            lat = 0
+            if 'geo' in data and data['geo'] != None and 'coordinates' in data['geo']:
+                lat = data['geo']['coordinates'][0]
+                lon = data['geo']['coordinates'][1]
+            else:
+                if 'place' in data and 'bounding_box' in data['place']:
+                    bbox = data['place']['bounding_box']['coordinates'][0]
+                    lon = (bbox[1][0] + bbox[2][0]) / 2.0
+                    lat = (bbox[0][1] + bbox[1][1]) / 2.0
+                elif 'location' in data and 'geo' in data['location']:
+                    bbox = data['location']['geo']['coordinates'][0]
+                    lon = (bbox[1][0] + bbox[2][0]) / 2.0
+                    lat = (bbox[0][1] + bbox[1][1]) / 2.0
+            T = Tweet(data['id_str'], data['text'], data['user']['id_str'], data['user']['name'], data['user']['followers_count'], time, lon, lat, words, keywords, hashtags, mentions_id, mentions_name, urls, favorite_count, retweet_count)
+            saveNewTweet(T)
 
     def on_error(self, status_code, data):
         print status_code
 
-ATL_BOUNDING_BOX_CORNERS = '-124.92,26.15,-66.59,48.96'
+BOUNDING_BOX = '-124.92,26.15,-66.59,48.96'
 
 def main():
-    with open('credentials.json') as f:
-        credentials = json.load(f)
+    f = open('credentials.json')
+    credentials = json.load(f)
+    f.close()
     stream = TweetSampler(
         credentials['APP_KEY'],
         credentials['APP_SECRET'],
         credentials['ACCESS_TOKEN'],
         credentials['ACCESS_TOKEN_SECRET'])
-    stream.statuses.filter(locations=ATL_BOUNDING_BOX_CORNERS)
+    stream.statuses.filter(language='en',locations=BOUNDING_BOX)
 
 if __name__ == '__main__':
     main()
--- twitter/tweet.py
+++ twitter/tweet.py
@@ -1,67 +1,74 @@
 from nltk.corpus import stopwords
 from collections import namedtuple
-import json, re
 from dbco import *
 
 global cacheToSave
 cacheToSave = []
 
 stopW = set(stopwords.words('english'))
-commW = ['http', 'like', 'ga', 'job', 'love', 'atlanta', 'amp', 'hiring', 'day', 'lol', 'know', 'time', 'want', 'got', 'good', 'shit', 'need', 'people', 'thank', 'today', 'really', 'make', 'careerarc', 'work', 'happy', 'great', 'going', 'nigga', 'think', 'come', 'new', 'life', 'school', 'right', 'll', 'feel', 'look', 'girl', 'night', 'thing', 'man', 'say', 've', 'friend', 'let', 'fuck', 'year', 'best', 'alway', 'way', 'wanna', 'tonight', 'game', 'better', 'birthday', 'mi', 'hate', 'latest', 'ain', 'im', 'tomorrow', 'u', 'gonna', 'home', 'god', 'getting', 'week', 'real', 'guy', 'oh', 'bad', 'sleep', 'morning', 'hope', 'tell', 'gotta', 'opening', 'bitch', 'said', 'click', 'baby', 'wait', 'ready', 'yall', 'boy', 'stop', 'didn', 'lmao', 'ya', 'start', 'damn', 'cause', 'talk', 'retail', 'team', 'follow', 'little', 'clas', 'mean', 'play']
+commW = []
 commW.extend(stopW)
 common_words = set(commW)
 
-Tweet = namedtuple('Tweet', ['guid', 'text', 'author', 'timestamp', 'lon', 'lat', 'words', 'hashtags', 'mentions'])
-class Tweet(namedtuple('Tweet', ['guid', 'text', 'author', 'timestamp', 'lon', 'lat', 'words', 'hashtags', 'mentions'])):
-    def __new__(cls, guid='', text='', author='', timestamp=0, lon=0, lat=0, words=[], hashtags=[], mentions=[]):
-        return super(Tweet, cls).__new__(cls, guid, text, author, timestamp, lon, lat, words, hashtags, mentions)
+Tweet = namedtuple('Tweet', ['guid', 'text', 'author_id', 'author_name', 'author_followers_count', 'timestamp', 'lon', 'lat', 'words', 'keywords', 'hashtags', 'mentions_id', 'mentions_name', 'urls', 'favorite_count', 'retweet_count'])
+class Tweet(namedtuple('Tweet', ['guid', 'text', 'author_id', 'author_name', 'author_followers_count', 'timestamp', 'lon', 'lat', 'words', 'keywords', 'hashtags', 'mentions_id', 'mentions_name', 'urls', 'favorite_count', 'retweet_count'])):
+    def __new__(cls, guid, text, author_id, author_name, author_followers_count, timestamp, lon, lat, words, keywords, hashtags, mentions_id, mentions_name, urls, favorite_count, retweet_count):
+        return super(Tweet, cls).__new__(cls, guid, text, author_id, author_name, author_followers_count, timestamp, lon, lat, words, keywords, hashtags, mentions_id, mentions_name, urls, favorite_count, retweet_count)
 
 def sendCache():
-	global cacheToSave
-	if len(cacheToSave) >= 30:
-		tweetInsert = db.tweet.initialize_unordered_bulk_op()
-		for T in cacheToSave:
-			tweetInsert.insert(T)
-		tweetInsert.execute()
-		cacheToSave = []
+    global cacheToSave
+    tweetInsert = db.tweet.initialize_unordered_bulk_op()
+    for T in cacheToSave:
+        tweetInsert.insert(T)
+    tweetInsert.execute()
+    cacheToSave = []
 
-def saveNewTweet(T, count):
-	global cacheToSave
-	if isValid(T, count):
-		cacheToSave.append(T._asdict())
-	if len(cacheToSave) >= 30:
-		sendCache()
+def saveNewTweet(T):
+    global cacheToSave
+    if isValid(T):
+        cacheToSave.append(T._asdict())
+    if len(cacheToSave) >= 100:
+        sendCache()
 
-def remove_non_ascii_1(text):
-	return ''.join(i for i in text if ord(i)<128)
-
-def cleanTweet(tweet):
-	global common_words
-	tweetString = re.sub('[()!\.,\?&;]', '', tweet).lower()
-	tweetString = remove_non_ascii_1(tweetString)
-	tweetString = tweetString.replace('\n', ' ')
-	words = tweetString.split(' ')
-	words = [w for w in words if ('http' not in w and len(w) > 1 and not w.isdigit())]
-	count = len(list(set(words)-common_words))
-	hashtags = [w for w in words if w.startswith('#')]
-	mentions = [w for w in words if w.startswith('@')]
-	words = list((set(words)-set(hashtags))-set(mentions))
-	return words, hashtags, mentions, count
+def summarizeTweet(text, hashtagsIndices, mentionsIndices, urlsIndices):
+    global common_words
+    startPoints = []
+    endPoints = []
+    for hIndices in hashtagsIndices:
+        startPoints.append(hIndices[0])
+        endPoints.append(hIndices[1] - 1)
+    for mIndices in mentionsIndices:
+        startPoints.append(mIndices[0])
+        endPoints.append(mIndices[1] - 1)
+    for uIndices in urlsIndices:
+        startPoints.append(uIndices[0])
+        endPoints.append(uIndices[1] - 1)
+    tweetString = ""
+    buf = ""
+    for i in range(len(text)):
+        if i in startPoints:
+            tweetString += buf
+        elif i in endPoints:
+            buf = ""
+        else:
+            if ord(text[i]) < 128:
+                if text[i] == '\n' or text[i] == '(' or text[i] == ')' or text[i] == '!' or text[i] == '.' or text[i] == ',' or text[i] == ';' or text[i] == '\"' or text[i] == '?' or text[i] == '&' or text[i] == '~' or text[i] == ':' or text[i] == '/':
+                    buf += ' '
+                else:
+                    buf += text[i]
+    tweetString += buf
+    rawwords = tweetString.split(' ')
+    words = []
+    for word in rawwords:
+        if word != "":
+            words.append(word.lower())
+    keywords = list(set(words)-common_words)
+    return words, keywords
 
 def insertTweet(T):
-	db.tweet.update({'guid': T['guid']}, {'$set': T}, upsert=True)
+    db.tweet.update({'guid': T['guid']}, {'$set': T}, upsert=True)
 
-def isValid(t, count):
-	# Verifies that a tweet has the minimal information needed
-	if t.guid == '':
-		return False
-	if t.text == '':
-		return False
-	if t.author == '':
-		return False
-	if t.timestamp < 500:
-		return False
-	myText = t.text
-	if count < 3:
-		return False
-	return True
+def isValid(t):
+    if len(t.words) + len(t.hashtags) + len(t.mentions_id) < 3:
+        return False
+    return True
